63b640a7f4c64ac5ed679a61a82b2c51e70e908a
anuj <someone@someplace.com>
2020-11-06 10:20:06 +0530

    first commit

--- calc/calc.go
+++ calc/calc.go
@@ -1,6 +1,12 @@
 package calc
 
+<<<<<<< HEAD
 // Add to add two numbers
 func Add(a, b int) int {
 	return a + b
+=======
+// Add add two numbers
+func Add(x, y int) int {
+	return x + y
+>>>>>>> first commit
 }
