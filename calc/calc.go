66277c5ae7eddf3357a8c0047d74be71b1279faa
anuj <someone@someplace.com>
2020-11-06 10:28:37 +0530

    zero edge is not allowed

--- calc/calc.go
+++ calc/calc.go
@@ -1,12 +1,6 @@
 package calc
 
-<<<<<<< HEAD
-// Add to add two numbers
-func Add(a, b int) int {
-	return a + b
-=======
 // Add add two numbers
 func Add(x, y int) int {
 	return x + y
->>>>>>> first commit
 }
--- geometry/geometry.go
+++
@@ -1,6 +0,0 @@
-package geometry
-
-// Cube finds volume of a cube
-func Cube(n int) int {
-	return n * n * n
-}
